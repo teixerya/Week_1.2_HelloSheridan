63a116ab0dbc43dc9d71f3ba4e4b51aeff779809
Ryan Teixeira <ryan.m.tex@gmail.com>
2021-05-20 13:47:44 -0400

    created empty project

---
+++ src/main/java/sheridan/teixerya/hellosheridan/HellosheridanApplication.java
@@ -0,0 +1,13 @@
+package sheridan.teixerya.hellosheridan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellosheridanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellosheridanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/teixerya/hellosheridan/HellosheridanApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.teixerya.hellosheridan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellosheridanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
