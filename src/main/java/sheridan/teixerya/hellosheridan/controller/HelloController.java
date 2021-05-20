c8115cfc95b64dc20a55457634b2fe4fc3e6d7ec
Ryan Teixeira <ryan.m.tex@gmail.com>
2021-05-20 15:17:02 -0400

    used @PathVariable in HelloController.java

--- src/main/java/sheridan/teixerya/hellosheridan/controller/HelloController.java
+++ src/main/java/sheridan/teixerya/hellosheridan/controller/HelloController.java
@@ -1,15 +1,25 @@
 package sheridan.teixerya.hellosheridan.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/Hello6")
-    public String hello(){
+    public String hello(@RequestParam(defaultValue = "Sheridan") String name, Model model){
 
+        model.addAttribute("name", name);
         return "Hello6";
     }
 
+
+    @GetMapping("/Hello6/{name}")
+    public String hello2(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "Hello6";
+    }
 }
